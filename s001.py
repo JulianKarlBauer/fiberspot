13852c9b0f35b29b900cbdc05d7089416d3fa2d8
julian karl bauer <juliankarlbauer@gmx.de>
2020-12-13 22:24:07 +0100

    Format script

--- s001.py
+++ s001.py
@@ -6,6 +6,10 @@ import numpy as np
 # Bands: R G B alpha
 # Modes: RGB, L
 
+
+directory = os.path.join("plots", "001")
+os.makedirs(directory, exist_ok=True)
+
 name_image = "IMG_9380.JPG"
 path = os.path.realpath(os.path.join("data", name_image))
 image = Image.open(path)
@@ -14,36 +18,36 @@ box = (300, 200, 900, 500)
 region = image.crop(box)
 
 region.save(
-    os.path.splitext(os.path.realpath(os.path.join("output", name_image)))[0]
-    + ".tiff"
+    os.path.splitext(os.path.realpath(os.path.join("output", name_image)))[0] + ".tiff"
 )
 red, green, blue = region.split()
 luminance = region.copy().convert("L")
 
-region_array = np.array(luminance)
+array = np.array(luminance)
 
 ########################################
 # Plot bands
 # Conclusion: Use luminance
 
-aspect_ratio = region_array.shape[0] / region_array.shape[1]
-fig, axs = plt.subplots(2, 2, figsize=(18, 18 * aspect_ratio))
-subplots = {
-    "Red": {"image": red, "ax": axs[0, 0]},
-    "Green": {"image": green, "ax": axs[0, 1]},
-    "Blue": {"image": blue, "ax": axs[1, 0]},
-    "Luminance": {"image": luminance, "ax": axs[1, 1]},
-}
+if False:
+    aspect_ratio = array.shape[0] / array.shape[1]
+    fig, axs = plt.subplots(2, 2, figsize=(18, 18 * aspect_ratio))
+    subplots = {
+        "Red": {"image": red, "ax": axs[0, 0]},
+        "Green": {"image": green, "ax": axs[0, 1]},
+        "Blue": {"image": blue, "ax": axs[1, 0]},
+        "Luminance": {"image": luminance, "ax": axs[1, 1]},
+    }
 
-directory = os.path.join("plots", "001")
-os.makedirs(directory, exist_ok=True)
-
-for key_sub, data_sub in subplots.items():
-    ax = data_sub["ax"]
-    ax.imshow(data_sub["image"])
-    ax.set_title(key_sub)
+    for key_sub, data_sub in subplots.items():
+        ax = data_sub["ax"]
+        ax.imshow(data_sub["image"])
+        ax.set_title(key_sub)
 
     path_picture = os.path.join(directory, f"modes")
+    plt.savefig(path_picture + ".png")
+    plt.tight_layout()
+    plt.close(fig)
 
     plt.savefig(path_picture + ".png")
 plt.tight_layout()
