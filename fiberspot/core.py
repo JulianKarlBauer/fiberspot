93a052a4739ebf3c41a3549f120fa5459974f951
julian karl bauer <juliankarlbauer@gmx.de>
2021-01-25 17:57:39 +0100

    Wrap into main function

--- fiberspot/core.py
+++ fiberspot/core.py
@@ -6,6 +6,7 @@ from PIL import Image, ImageOps
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.interpolate
+import fiberspot
 
 
 def load_and_convert_image(path):
@@ -151,3 +152,101 @@ class LocalFiberVolumeContentMap:
 
     def __call__(self, value):
         return self.interpolate(value)
+
+
+def get_local_fiber_volume_content(properties):
+
+    directory = properties["plot_directory"]
+    os.makedirs(directory, exist_ok=True)
+
+    plot = properties["plot"]
+
+    ########################################
+    # Load image and reformat
+
+    # Load
+    raw_images = {
+        key: fiberspot.load_and_convert_image(path=properties[key]["path"])
+        for key in ["specimen", "neat_resin"]
+    }
+
+    # Crop
+    images = {
+        key: raw.crop(box=properties[key]["box"]) if "box" in properties[key] else raw
+        for key, raw in raw_images.items()
+    }
+
+    image_arrays = {key: np.array(image) for key, image in images.items()}
+
+    # Plot
+    if plot:
+        fiberspot.plot_bands_of_image(
+            path=properties["specimen"]["path"],
+            box=properties["specimen"]["box"],
+            plot_directory=directory,
+        )
+
+    ########################################
+    # Grid
+    grid_xx, grid_yy = fiberspot.get_regular_grid_on_image(
+        array=image_arrays["specimen"]
+    )
+
+    # Plot
+    if plot:
+        fiberspot.plot_grid(
+            array=image_arrays["specimen"],
+            grid_xx=grid_xx,
+            grid_yy=grid_yy,
+            plot_directory=directory,
+        )
+
+    ########################################
+    # Local fiber volume content
+    fvc_map = fiberspot.LocalFiberVolumeContentMap(
+        average_grey=np.mean(image_arrays["specimen"]),
+        average_volume_content=properties["average_volume_content_specimen"],
+        neat_grey=np.mean(image_arrays["neat_resin"]),
+    )
+
+    # Plot
+    if plot:
+        fiberspot.plot_fiber_volume_content(fvc_map, plot_directory=directory)
+
+    ########################################
+    # Create masks and calc local fiber volume content on specific areas
+
+    radius = properties["radius"]
+    if plot:
+        fiberspot.plot_mask(
+            array=image_arrays["specimen"],
+            grid_xx=grid_xx,
+            grid_yy=grid_yy,
+            radius=radius,
+            plot_directory=directory,
+        )
+
+    means = np.zeros_like(grid_xx)
+    fvcs = np.zeros_like(grid_xx)
+    for i in range(10):
+        for j in range(10):
+            mask = fiberspot.create_single_circular_mask(
+                image_shape_2D=image_arrays["specimen"].shape[:2],
+                center=(grid_xx[i, j], grid_yy[i, j]),
+                radius=radius,
+            )
+            mean = image_arrays["specimen"][mask].mean()
+            means[i, j] = mean
+            fvcs[i, j] = fvc_map(value=mean)
+
+    if plot:
+        fiberspot.plot_image(
+            image=means,
+            title="Mean values",
+            path=os.path.join(directory, "means" + ".png"),
+        )
+        fiberspot.plot_image(
+            image=fvcs,
+            title="Fiber volume content",
+            path=os.path.join(directory, "fvcs" + ".png"),
+        )
--- s003.py
+++ s003.py
@@ -2,12 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import os
-import numpy as np
 import fiberspot
 
 
 images = {
-    "knips_04": {
+    "example": {
         "specimen": {
             "path": os.path.realpath(os.path.join("data", "example.JPG")),
             "box": (1340, 360, 2700, 1700),
@@ -18,89 +17,10 @@ images = {
         },
         "radius": 60,
         "average_volume_content_specimen": 0.27,
+        "plot": True,
+        "plot_directory": os.path.join("plots", "example"),
     },
 }
 
-for key, properties in images.items():
-    directory = os.path.join("plots", key)
-    os.makedirs(directory, exist_ok=True)
 
-    ########################################
-    # Load image and reformat
-
-    # Load
-    raw_images = {
-        key: fiberspot.load_and_convert_image(path=properties[key]["path"])
-        for key in ["specimen", "neat_resin"]
-    }
-
-    # Crop
-    images = {key: raw.crop(properties[key]["box"]) for key, raw in raw_images.items()}
-
-    image_arrays = {key: np.array(image) for key, image in images.items()}
-
-    # Plot
-    fiberspot.plot_bands_of_image(
-        path=properties["specimen"]["path"],
-        box=properties["specimen"]["box"],
-        plot_directory=directory,
-    )
-
-    ########################################
-    # Grid
-    grid_xx, grid_yy = fiberspot.get_regular_grid_on_image(
-        array=image_arrays["specimen"]
-    )
-
-    # Plot
-    fiberspot.plot_grid(
-        array=image_arrays["specimen"],
-        grid_xx=grid_xx,
-        grid_yy=grid_yy,
-        plot_directory=directory,
-    )
-
-    ########################################
-    # Local fiber volume content
-    fvc_map = fiberspot.LocalFiberVolumeContentMap(
-        average_grey=np.mean(image_arrays["specimen"]),
-        average_volume_content=properties["average_volume_content_specimen"],
-        neat_grey=np.mean(image_arrays["neat_resin"]),
-    )
-
-    # Plot
-    fiberspot.plot_fiber_volume_content(fvc_map, plot_directory=directory)
-
-    ########################################
-    # Create masks and calc local fiber volume content on specific areas
-
-    radius = properties["radius"]
-    fiberspot.plot_mask(
-        array=image_arrays["specimen"],
-        grid_xx=grid_xx,
-        grid_yy=grid_yy,
-        radius=radius,
-        plot_directory=directory,
-    )
-
-    means = np.zeros_like(grid_xx)
-    fvcs = np.zeros_like(grid_xx)
-    for i in range(10):
-        for j in range(10):
-            mask = fiberspot.create_single_circular_mask(
-                image_shape_2D=image_arrays["specimen"].shape[:2],
-                center=(grid_xx[i, j], grid_yy[i, j]),
-                radius=radius,
-            )
-            mean = image_arrays["specimen"][mask].mean()
-            means[i, j] = mean
-            fvcs[i, j] = fvc_map(value=mean)
-
-    fiberspot.plot_image(
-        image=means, title="Mean values", path=os.path.join(directory, "means" + ".png")
-    )
-    fiberspot.plot_image(
-        image=fvcs,
-        title="Fiber volume content",
-        path=os.path.join(directory, "fvcs" + ".png"),
-    )
+fiberspot.get_local_fiber_volume_content(properties=images["example"])
