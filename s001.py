592f114a20db36732918ddd70e2cb95e4757b557
julian karl bauer <juliankarlbauer@gmx.de>
2021-01-26 16:46:29 +0100

    Give morphiological cv a try: is not clear...

--- s001.py
+++ s001.py
@@ -9,7 +9,8 @@ import scipy.interpolate
 from skimage import img_as_float
 import fiberspot
 from fiberspot import example_script
-from skimage.segmentation import chan_vese
+from skimage.segmentation import chan_vese, morphological_chan_vese
+from skimage import segmentation
 
 
 def plot_segmentation(segmentation_result, image_array, plot_directory):
@@ -23,17 +24,8 @@ def plot_segmentation(segmentation_result, image_array, plot_directory):
     ax[0].set_axis_off()
     ax[0].set_title("Original Image", fontsize=12)
 
-    ax[1].imshow(segmentation_result[0], cmap="gray")
+    ax[1].imshow(segmentation_result, cmap="gray")
     ax[1].set_axis_off()
-    title = "Chan-Vese segmentation - {} iterations".format(len(segmentation_result[2]))
-    ax[1].set_title(title, fontsize=12)
-
-    ax[2].imshow(segmentation_result[1], cmap="gray")
-    ax[2].set_axis_off()
-    ax[2].set_title("Final Level Set", fontsize=12)
-
-    ax[3].plot(segmentation_result[2])
-    ax[3].set_title("Evolution of energy over iterations", fontsize=12)
 
     fig.tight_layout()
     path_picture = os.path.join(plot_directory, f"segmentation")
@@ -50,16 +42,25 @@ original = Image.open(arguments["specimen"]["path"])
 specimen = original.crop(box=arguments["specimen"]["box"])
 specimen_grey = ImageOps.invert(specimen.convert("L"))
 image_array = img_as_float(specimen_grey)
+#
+# segmentation_result = chan_vese(
+#     image_array,
+#     mu=0.7,
+#     lambda1=1.1,
+#     lambda2=0.9,
+#     tol=1e-3,
+#     max_iter=200,
+#     dt=0.2,
+#     init_level_set="checkerboard",
+#     extended_output=True,
+# )
 
-segmentation_result = chan_vese(
-    image_array,
-    mu=0.25,
-    lambda1=1,
-    lambda2=1,
-    tol=1e-3,
-    max_iter=200,
-    dt=0.5,
-    init_level_set="checkerboard",
-    extended_output=True,
+initial_level_set = np.zeros(image_array.shape)
+initial_level_set[[0, -1], :] = 1
+initial_level_set[:, [0, -1]] = 1
+segmentation_result = morphological_chan_vese(
+    image_array, 35, init_level_set=initial_level_set, smoothing=3
 )
+
+
 plot_segmentation(segmentation_result, image_array, plot_directory)
