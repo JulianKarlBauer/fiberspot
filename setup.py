b936c6d391b5d89553e27db040aa36fff2ad51b3
julian karl bauer <juliankarlbauer@gmx.de>
2021-01-26 13:32:22 +0100

    Clean and start with python 3.7 for scipy

--- setup.py
+++ setup.py
@@ -12,36 +12,9 @@ setuptools.setup(
     url="https://git.scc.kit.edu/julian_/fiberspot",
     packages=["fiberspot"],
     package_dir={"fiberspot": "fiberspot"},
-    # package_data={
-    #     "fiberspot": package_files(
-    #         directory=os.path.realpath(
-    #             os.path.join(
-    #                 os.path.dirname(os.path.realpath(__file__)), "fiberspot", "data"
-    #             )
-    #         )
-    #     )
-    # },
-    # entry_points={
-    #     "console_scripts": [
-    #         "export_data_sets_to_file = fiberspot.scripts:export_data_sets_to_file",
-    #     ]
-    # },
-    # include_package_data=True,
     install_requires=["numpy", "matplotlib", "pillow", "scipy", "pytest"],
-    # extras_require={":python_version>'3.5'": ["natsort", "matplotlib"]},
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
 )
-
-# import os
-#
-#
-# def package_files(directory, exclude_pattern="__pycache__"):
-#     paths = []
-#     for (path, directories, filenames) in os.walk(directory):
-#         for filename in filenames:
-#             if exclude_pattern not in path:
-#                 paths.append(os.path.join("..", path, filename))
-#     return paths
