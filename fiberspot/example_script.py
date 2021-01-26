fc51bbd12e2440dce5b4cac35754e51b715035d7
julian karl bauer <juliankarlbauer@gmx.de>
2021-01-26 16:39:52 +0100

    Define path from example script

--- fiberspot/example_script.py
+++ fiberspot/example_script.py
@@ -4,13 +4,19 @@
 import os
 import fiberspot
 
+path_this_files_dir = os.path.realpath(os.path.dirname(os.path.dirname(__file__)))
+
 arguments = {
     "specimen": {
-        "path": os.path.realpath(os.path.join("data", "example.jpg")),
+        "path": os.path.realpath(
+            os.path.join(path_this_files_dir, "data", "example.jpg")
+        ),
         "box": (1340, 360, 2700, 1700),  # Optional cropping
     },
     "neat_resin": {
-        "path": os.path.realpath(os.path.join("data", "example.jpg")),
+        "path": os.path.realpath(
+            os.path.join(path_this_files_dir, "data", "example.jpg")
+        ),
         "box": (3230, 2220, 4650, 3550),  # Optional cropping
     },
     "radius": 60,  # pixel
